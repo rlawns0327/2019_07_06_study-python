ad3fd35c059ed0063bf6311fb9a7cfc2679a28a3
rlawns0327 <rlawns0327@naver.com>
2019-07-06 17:25:56 +0900

    blog.Post 모델을 admin에 등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+#admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    list_filter = ['updatded_at']
+    search_fields = ['title']
